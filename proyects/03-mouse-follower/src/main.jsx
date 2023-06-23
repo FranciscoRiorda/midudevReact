190032931de68b75492bf80a32a2aae658794968
Francisco Riorda <riorda7@gmail.com>
2023-06-23 20:01:08 -0300

    followerMouse

--- proyects/03-mouse-follower/src/main.jsx
+++ proyects/03-mouse-follower/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    {/* StrictMode: No funciona en producción, es para que tengamos codigo actualizado y nos ayuda a depurar el código, 
+     ver si tenemos errores al ejecutar la app. */}
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>
+);
