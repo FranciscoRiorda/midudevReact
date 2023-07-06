f6e0aa2a505d78ead54d62cbcfeaf93fb208f76e
Francisco Riorda <riorda7@gmail.com>
2023-07-06 14:46:07 -0300

    fetch

--- proyects/05-react-buscador-peliculas/src/App.jsx
+++ proyects/05-react-buscador-peliculas/src/App.jsx
@@ -6,12 +6,12 @@ import { useSearch } from "./hooks/useSearch";
 
 
 function App() {
-  const { movies } = useMovies();
   const {query, setQuery, error} = useSearch()
+  const { movies, getMovies } = useMovies({query});
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log({ query });
+    getMovies()
   };
 
   const handleChange = (event) => {
--- proyects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ proyects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,15 +1,32 @@
-import responseMovies from "../mocks/results.json";
+import resultsMovies from "../mocks/results.json";
 import noResponseMovies from "../mocks/no-results.json";
+import { useState } from "react";
 
-export function useMovies() {
-    const movies = responseMovies.Search;
-  
-    const mappedMovies = movies?.map((movie) => ({
-      id: movie.imdbID,
-      title: movie.Title,
-      year: movie.Year,
-      poster: movie.Poster,
-    }));
-  
-    return {movies: mappedMovies}
-  }
+export function useMovies({ query }) {
+  const [responseMovies, setResponseMovies] = useState([]);
+
+  const movies = responseMovies.Search;
+
+  const mappedMovies = movies?.map((movie) => ({
+    id: movie.imdbID,
+    title: movie.Title,
+    year: movie.Year,
+    poster: movie.Poster,
+  }));
+
+  const getMovies = () => {
+    if (query) {
+      fetch(`https://www.omdbapi.com/?apikey=5e8bf6a7&s=${query}`)
+      .then( res => res.json())
+      .then(json => {
+        setResponseMovies(json)
+      })
+      
+      
+    } else {
+      setResponseMovies(noResponseMovies);
+    }
+  };
+
+  return { movies: mappedMovies, getMovies };
+}
