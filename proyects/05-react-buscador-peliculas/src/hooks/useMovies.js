8cac3bd977724e3feea78497af81a640e02ead45
Francisco Riorda <riorda7@gmail.com>
2023-07-07 15:13:45 -0300

    Evitar misma búsqueda

--- proyects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ proyects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,20 +1,24 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { searchMovies } from "../services/moviesService";
 
 export function useMovies({ query }) {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const previousSearch = useRef(query);
 
   const getMovies = async () => {
+    if (query === previousSearch.current) return;
     try {
       setLoading(true);
       setError(null);
+      previousSearch.current = query;
       const newMovies = await searchMovies({ query });
       setMovies(newMovies);
     } catch (e) {
       setError(e.message);
-    } finally { //Se ejecuta tanto si pasa por el try como por el catch
+    } finally {
+      //Se ejecuta tanto si pasa por el try como por el catch
       setLoading(false);
     }
   };
