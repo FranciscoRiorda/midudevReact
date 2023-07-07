ad478a69fb404428c136370fbf436b0cb482e1f6
Francisco Riorda <riorda7@gmail.com>
2023-07-07 16:04:56 -0300

    useCallback

--- proyects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ proyects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,4 +1,4 @@
-import { useMemo, useRef, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import { searchMovies } from "../services/moviesService";
 
 export function useMovies({ query, sort }) {
@@ -7,8 +7,10 @@ export function useMovies({ query, sort }) {
   const [error, setError] = useState(null);
   const previousSearch = useRef(query);
 
-  const getMovies = useMemo(() => { // Este useMemo sirve para no ejecutar getMovies cada vez que se cambia el sort en el buscador. Para guardar un valor cuando esa dependencia cambie.
-    return async ({query}) => {
+  const getMovies = useCallback(
+    // Este useMemo sirve para no ejecutar getMovies cada vez que se cambia el sort en el buscador. Para guardar un valor cuando esa dependencia cambie.
+    // useCallback se utiliza para abreviar la sintaxis del useMemo. useMemo(() =>{},[]). useCallback(,[]). Se usan sólo para funciones dentro de funciones.
+    async ({ query }) => {
       if (query === previousSearch.current) return;
       try {
         setLoading(true);
@@ -22,10 +24,11 @@ export function useMovies({ query, sort }) {
         //Se ejecuta tanto si pasa por el try como por el catch
         setLoading(false);
       }
-    };
-  },[]) 
+    },[]
+  );
 
-  const sortedMovies = useMemo(() => { //useMemo para guardar/memorizar algún elemento. No usar siempre, en cosas simple hacer calculo directamente
+  const sortedMovies = useMemo(() => {
+    //useMemo para guardar/memorizar algún elemento. No usar siempre, en cosas simple hacer calculo directamente
     return sort
       ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
       : movies;
